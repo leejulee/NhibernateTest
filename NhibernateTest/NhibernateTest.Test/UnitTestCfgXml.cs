aa15a3c0e97cfb84bb7855cba24ba8342354ce89
leoli <cvsf04456@yahoo.com.tw>
2016-03-21 23:04:08 +0800

    add cfg.xml  setting & cfg.xml info

---
+++ NhibernateTest/NhibernateTest.Test/UnitTestCfgXml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+//using NHibernate.Bytecode;
+//using NHibernate.Dialect;
+//using NHibernate.Driver;
+
+namespace NhibernateTest.Test
+{
+    [TestClass]
+    public class UnitTestCfgXml
+    {
+        [TestMethod]
+        public void TestConfigMappingByCode()
+        {
+            var config = new NHibernate.Cfg.Configuration();
+            config.Configure();
+            config.DataBaseIntegration(db =>
+            {
+                db.KeywordsAutoImport = Hbm2DDLKeyWords.AutoQuote;
+            });
+
+            var mapper = new ModelMapper();
+            mapper.AddMapping(typeof(UserEntityMap));
+            mapper.AddMapping(typeof(AdminEntityMap));
+            mapper.AddMapping(typeof(FileEntityMap));
+            mapper.AddMapping(typeof(ImageEntityMap));
+            mapper.AddMapping(typeof(VideoEntityMap));
+            mapper.AddMapping(typeof(MessageEntityMap));
+            mapper.AddMapping(typeof(CommentEntityMap));
+            var maps = mapper.CompileMappingForAllExplicitlyAddedEntities();
+            config.AddDeserializedMapping(maps, "Models");
+
+            var factory = config.BuildSessionFactory();
+
+        }
+    }
+}
